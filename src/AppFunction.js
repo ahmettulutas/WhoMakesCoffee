6d927f8a8180bb0da9a8953dc179aa93649a2f45
Ahmet Ulutaş <englishlectures.stocks@gmail.com>
2021-10-24 22:23:48 +0300

    made some changes

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,19 +1,17 @@
 import { React,  useState } from "react";
 import "./styles.css";
 export default function App() {
-  const [skorlar, setSkorlar] = useState([{TANSU:0, allScore : 0},{AHMET:0, allScore : 0},{OZAN:0, allScore : 0}]);
+  const [scores, setScores] = useState([{TANSU:0, allScore : 0},{AHMET:0, allScore : 0},{OZAN:0, allScore : 0}]);
   const [result, setResult] = useState(0);
 
 
 
   const handleClick = () =>  {
-    const newObj = JSON.parse(JSON.stringify(skorlar))
+    const newObj = JSON.parse(JSON.stringify(scores))
     newObj.map(item => (item.TANSU = Math.floor(Math.random() * 100), item.AHMET = Math.floor(Math.random() * 100), item.OZAN = Math.floor(Math.random() * 100))
       )
-    setSkorlar(newObj )
+    setScores(newObj )
     const max = Object.keys(newObj[0]).reduce((a,b)=> newObj[0][a]> newObj[0][b] ? a : b)
-    console.log(max)
-    console.log(newObj[0].TANSU)
     if(max === "TANSU"){
       newObj[0].allScore ++;
     }
@@ -24,30 +22,16 @@ export default function App() {
       newObj[2].allScore ++;
     }
     setResult(max)
-    setSkorlar(newObj)
+    setScores(newObj)
   };
   
-    /*const handleClick = () => {
-              setSkorlar((prev) => ({
-                ...prev,
-                TANSU: Math.floor(Math.random() * 100)
-              }));
-              setSkorlar((prev) => ({
-                ...prev,
-                AHMET: Math.floor(Math.random() * 100)
-              }));
-              setSkorlar((prev) => ({
-                ...prev,
-                OZAN: Math.floor(Math.random() * 100)
-              }));
-            console.log(JSON.stringify(skorlar))
-            */
+
   return (
     <div className="mainDiv">
-      <h1>{result} wins !!!</h1>
-      <p>Tansu: {skorlar[0].TANSU}</p>
-      <p>Ahmet: {skorlar[0].AHMET}</p>
-      <p>Ozan: {skorlar[0].OZAN}</p>
+      <h1>{result ? `${result} wins` : "LET'S GO"}  !!!</h1>
+      <p>Tansu: {scores[0].TANSU}</p>
+      <p>Ahmet: {scores[0].AHMET}</p>
+      <p>Ozan: {scores[0].OZAN}</p>
       <button className="button" onClick={handleClick}>
         Click Me
       </button>
@@ -59,9 +43,9 @@ export default function App() {
           <th>Ozan</th>
           </tr>
           <tr>
-          <td>{skorlar[0].allScore}</td>
-          <td>{skorlar[1].allScore}</td>
-          <td>{skorlar[2].allScore}</td>
+          <td>{scores[0].allScore}</td>
+          <td>{scores[1].allScore}</td>
+          <td>{scores[2].allScore}</td>
           </tr>
         </table>
       </div>
