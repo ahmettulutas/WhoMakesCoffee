e3599ca6e4246ba10a5490354205f846f1619fd1
Ahmet Ulutaş <englishlectures.stocks@gmail.com>
2021-10-31 16:36:19 +0300

    new version of the game with better styling

--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,58 +1,95 @@
-import { React,  useState } from "react";
 import "./styles.css";
+import { useState } from "react";
+import img from "./arcade-background.jpeg";
 export default function App() {
-  const [scores, setScores] = useState([{TANSU:0, allScore : 0},{AHMET:0, allScore : 0},{OZAN:0, allScore : 0}]);
-  const [result, setResult] = useState(0);
-
-
-
-  const handleClick = () =>  {
-    const newObj = JSON.parse(JSON.stringify(scores))
-    newObj.map(item => (item.TANSU = Math.floor(Math.random() * 100), item.AHMET = Math.floor(Math.random() * 100), item.OZAN = Math.floor(Math.random() * 100))
-      )
-    setScores(newObj )
-    const max = Object.keys(newObj[0]).reduce((a,b)=> newObj[0][a]> newObj[0][b] ? a : b)
-    if(max === "TANSU"){
-      newObj[0].allScore ++;
+  const colors = [{ red: "#FF0000", yellow: "#ffcc00", green: "#339900" }];
+  const [scores, setScores] = useState({
+    scores: { Tansu: 0, Ahmet: 0, Ozan: 0 },
+    totalScores: { Tansu: 0, Ahmet: 0, Ozan: 0 },
+    max: null
+  });
+  const setWarningColor = (point) => {
+    if (point <= 1) {
+      return colors[0].red;
+    } else if (point <= 2) {
+      return colors[0].yellow;
+    } else {
+      return colors[0].green;
     }
-    else if (max === "AHMET") {
-      newObj[1].allScore ++;
-    }
-    else {
-      newObj[2].allScore ++;
-    }
-    setResult(max)
-    setScores(newObj)
   };
-  
-
+  const handleScores = () => {
+    const newObj = JSON.parse(JSON.stringify(scores));
+    newObj.scores.Tansu = Math.floor(Math.random() * 100);
+    newObj.scores.Ahmet = Math.floor(Math.random() * 100);
+    newObj.scores.Ozan = Math.floor(Math.random() * 100);
+    newObj.max = Object.keys(newObj.scores).reduce((a, b) =>
+      newObj.scores[a] === newObj.scores[b]
+        ? "nobody"
+        : newObj.scores[a] > newObj.scores[b]
+        ? a
+        : b
+    );
+    // the following conditions set the total score
+    if (newObj.max === "Tansu") {
+      newObj.totalScores.Tansu++;
+    } else if (newObj.max === "Ahmet") {
+      newObj.totalScores.Ahmet++;
+    } else if (newObj.max === "Ozan") {
+      newObj.totalScores.Ozan++;
+    }
+    setScores(newObj);
+  };
   return (
-    <div className="main-div">
+    <div style={{ backgroundImage: `url(${img})` }} className="main-div">
       <div className="flex-div">
         <div className="round-scores-div">
-      <h1>{result ? `${result} wins` : "LET'S GO"}  !!!</h1>
-      <p>Tansu: {scores[0].TANSU}</p>
-      <p>Ahmet: {scores[0].AHMET}</p>
-      <p>Ozan: {scores[0].OZAN}</p>
-      <button className="button" onClick={handleClick}>
-        Click Me
-      </button>
-      </div>
-      <div className="table-div">
-        <h1>TOTAL SCORES</h1>
-        <table>
-          <tr>
-          <th>Tansu</th>
-          <th>Ahmet</th>
-          <th>Ozan</th>
-          </tr>
-          <tr>
-          <td>{scores[0].allScore}</td>
-          <td>{scores[1].allScore}</td>
-          <td>{scores[2].allScore}</td>
-          </tr>
-        </table>
-      </div>
+          <h1>{scores.max ? `${scores.max} wins` : "LET'S GO"} !!!</h1>
+          <p>Tansu: {scores.scores.Tansu}</p>
+          <p>Ahmet: {scores.scores.Ahmet}</p>
+          <p>Ozan: {scores.scores.Ozan}</p>
+          <button className="button" onClick={handleScores}>
+            Play
+          </button>
+        </div>
+        <fieldset className="table-div">
+          <legend>TOTAL SCORES</legend>
+          <table>
+            <tr>
+              <td>Tansu</td>
+              <td>Ahmet</td>
+              <td>Ozan</td>
+            </tr>
+            <tr>
+              <td
+                style={{
+                  "background-color": `${setWarningColor(
+                    scores.totalScores.Tansu
+                  )}`
+                }}
+              >
+                {scores.totalScores.Tansu}
+              </td>
+              <td
+                style={{
+                  "background-color": `${setWarningColor(
+                    scores.totalScores.Ahmet
+                  )}`
+                }}
+              >
+                {scores.totalScores.Ahmet}
+              </td>
+              <td
+                style={{
+                  "background-color": `${setWarningColor(
+                    scores.totalScores.Ozan
+                  )}`
+                }}
+              >
+                {scores.totalScores.Ozan}
+              </td>
+            </tr>
+          </table>
+        </fieldset>
       </div>
     </div>
   );
